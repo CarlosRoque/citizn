d164a1571ab5c77426b8865e4f19bc421cfe7899
Carlos Roque <carlos.roque@bayphoto.com>
2018-02-08 12:05:35 -0600

    Work Started. added bootstraped irb

--- Rakefile
+++ Rakefile
@@ -7,4 +7,7 @@ Rake::TestTask.new(:test) do |t|
   t.test_files = FileList["test/**/*_test.rb"]
 end
 
-task :default => :test
+# DEFAULT
+task :default do
+    exec("irb -r ./lib/irb/bootstrap.rb")
+end
---
+++ lib/irb/bootstrap.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'yaml'
+require 'fileutils'
+require 'uri'
+# require 'require_all'
+
+# common
+require './test/test_helper'
+
+################################
+# DEBUGGING
+################################
+puts "citizn"
